4501e12171a0fe36c6f8d49f7644ff867ced8982
Andrew <vandyand@gmail.com>
2020-02-26 23:12:03 -0500

    added media queries for smaller screen layout

--- src/components/List.js
+++ src/components/List.js
@@ -11,8 +11,9 @@ export default class extends React.Component {
         return (
             <div className="listDisplay">
                 {/*List Title*/}
-                {this.props.state.lists.filter(val => val._id === this.props.state.list_id)
-                    .map(val => <h1 key={val._id}>{val.name}</h1>)}
+                {/* {this.props.state.lists.filter(val => val._id === this.props.state.list_id)
+                    .map(val => <h1 key={val._id}>{val.name}</h1>)} */}
+                <h1>Todo Items</h1>
                 {/*List Description*/}
                 {this.props.state.lists.filter(val => val._id === this.props.state.list_id)
                     .map(val => (val.desc ? <h2 key={val._id}>{val.desc}</h2> : ''))}
